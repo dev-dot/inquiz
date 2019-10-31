5fb6f3ff6716a9d2e37d1ee35c2d30856bb531bf
Anton Richter <ar140@hdm-stuttgart.de>
2019-10-31 17:25:04 +0100

    new Interface "IGamemode", "IJoker".

---
+++ src/main/java/de/hdm_stuttgart/mi/interfaces/IGamemode.java
@@ -0,0 +1,7 @@
+package de.hdm_stuttgart.mi.interfaces;
+
+public interface IGamemode {
+    void standardMode();
+    void speedMode();
+    void expertMode();
+}
---
+++ src/main/java/de/hdm_stuttgart/mi/interfaces/IJoker.java
@@ -0,0 +1,5 @@
+package de.hdm_stuttgart.mi.interfaces;
+
+public interface IJoker {
+    void useJoker();
+}
