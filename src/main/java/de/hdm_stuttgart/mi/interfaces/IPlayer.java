15c29030d762cb7afa04899a1ea9c106b81a2a28
Anton Richter <ar140@hdm-stuttgart.de>
2019-10-31 16:51:05 +0100

    Update interface

---
+++ src/main/java/de/hdm_stuttgart/mi/interfaces/IPlayer.java
@@ -0,0 +1,11 @@
+package de.hdm_stuttgart.mi.interfaces;
+
+public interface IPlayer {
+
+    void startGame();
+    void selectAnswer();
+    void checkStats();
+    void selectJoker();
+    void endGame();
+
+}
