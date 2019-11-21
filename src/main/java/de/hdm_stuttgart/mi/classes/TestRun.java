d6b0a419b283029effe3b9581bc620357a0b745c
Anton Richter <ar140@hdm-stuttgart.de>
2019-11-21 10:46:59 +0100

    created new branch without Gui

--- src/main/java/de/hdm_stuttgart/mi/classes/TestRun.java
+++ src/main/java/de/hdm_stuttgart/mi/classes/TestRun.java
@@ -5,26 +5,29 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
+import static javax.xml.bind.JAXBContext.newInstance;
+
 public class TestRun {
 
     public static void main(String[] args){
-        Quiz quiz = null;
+        Quiz quiz;
 
 
-        File file = new File("src/main/java/Test/question.xml");
+        File file = new File("src/main/resources/content/questions.xml");
 
 
 
         {
             try {
-                JAXBContext jaxbContext = JAXBContext.newInstance(Quiz.class);
+                JAXBContext jaxbContext = newInstance(Quiz.class);
                 Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
                 quiz = (Quiz) unmarshaller.unmarshal(file);
+                System.out.println(quiz.getQuestions().get(0).getAnswer());
+
+
             } catch (JAXBException e) {
                 e.printStackTrace();
             }
-
-            System.out.println(quiz.getQuestions().get(0).getAnswer());
 
         }
 
--- src/main/java/de/hdm_stuttgart/mi/classes/testParser.java
+++ src/main/java/de/hdm_stuttgart/mi/classes/testParser.java
@@ -1,16 +1,19 @@
 package de.hdm_stuttgart.mi.classes;
 
 import java.io.File;
-import javax.xml.parsers.*;
-import org.w3c.dom.Document;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class testParser{
 
-    public static void main( String[] args ) throws Exception
-    {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse( new File("src/main/resources/content/questions.xml") );
-        System.out.println( document.getFirstChild().getTextContent() );
+public class testParser {
+
+    public static void main(String[] args) {
+
+
+
+
     }
 }
