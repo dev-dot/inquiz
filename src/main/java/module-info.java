943b9ebb15f3c5e0267a16420913c4a18f834aba
Filippo Matraxia <fm080@hdm-stuttgart.de>
2019-12-01 12:54:06 +0100

    Hopefully cleaned up the mess from the last commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module inquiz {
     requires java.xml.bind;
+    opens de.hdm_stuttgart.mi.classes to java.xml.bind;
+
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.graphics;
     requires org.apache.logging.log4j.core;
     requires org.apache.logging.log4j;
