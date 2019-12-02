e04505acfdfc345ffa2bf19990f637226e283b5f
Schwabe Kai <ks204@hdm-stuttgart.de>
2019-12-02 21:31:52 +0100

    cleaned up MainController cleaned up StartWindow.fxml

--- src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
+++ src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
@@ -1,12 +1,9 @@
 package de.hdm_stuttgart.mi.Gui.ViewHandler;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainController extends Application {
@@ -16,16 +13,8 @@ public class MainController extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/StartWindow.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/Style/default.css").toExternalForm());
-        scene.widthProperty().addListener(new ChangeListener<Number>() {
-            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
-                System.out.println("Width: " + newSceneWidth);
-            }
-        });
-        scene.heightProperty().addListener(new ChangeListener<Number>() {
-            @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
-                System.out.println("Height: " + newSceneHeight);
-            }
-        });
+        scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> System.out.println("Width: " + newSceneWidth));
+        scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> System.out.println("Height: " + newSceneHeight));
         primaryStage.setTitle("inquiz");
         primaryStage.setMinWidth(340);
         primaryStage.setMinHeight(300);
