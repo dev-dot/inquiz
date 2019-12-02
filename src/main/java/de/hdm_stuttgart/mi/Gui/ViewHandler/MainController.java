0d633b0ac278aa258abedd7b447409112b3a5537
Schwabe Kai <ks204@hdm-stuttgart.de>
2019-12-02 21:49:47 +0100

    maybe resolved Font loading issue ?

--- src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
+++ src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
@@ -12,6 +12,7 @@ public class MainController extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/StartWindow.fxml"));
         Scene scene = new Scene(root);
+        javafx.scene.text.Font.loadFont("fonts/Source_Code_Pro/SourceCodePro-ExtraLight.ttf", 200);
         scene.getStylesheets().add(getClass().getResource("/Style/default.css").toExternalForm());
         scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> System.out.println("Width: " + newSceneWidth));
         scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> System.out.println("Height: " + newSceneHeight));
