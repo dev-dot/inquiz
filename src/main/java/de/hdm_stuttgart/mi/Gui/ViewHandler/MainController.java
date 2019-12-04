c09be220689abb8c1a20caf25b83dbe5ece6747d
Anton Richter <ar140@hdm-stuttgart.de>
2019-12-04 17:36:10 +0100

    [Class] changed the path to load the Font

--- src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
+++ src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
@@ -2,8 +2,10 @@ package de.hdm_stuttgart.mi.Gui.ViewHandler;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,7 +18,7 @@ public class MainController extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/StartWindow.fxml"));
         Scene scene = new Scene(root);
-        javafx.scene.text.Font.loadFont("fonts/Source_Code_Pro/SourceCodePro-ExtraLight.ttf", 200);
+        javafx.scene.text.Font.loadFont(getClass().getResourceAsStream("/Style/fonts/Source_Code_Pro/SourceCodePro-ExtraLight.ttf"), 14);
         scene.getStylesheets().add(getClass().getResource("/Style/default.css").toExternalForm());
         scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> log.info("Width: " + newSceneWidth));
         scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> log.info("Height: " + newSceneHeight));
