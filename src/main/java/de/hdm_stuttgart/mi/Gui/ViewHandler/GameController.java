250bdcee0739805f40e059f32c1c3802b6864f7e
Anton Richter <ar140@hdm-stuttgart.de>
2019-12-04 21:45:19 +0100

    [CLASS] bugfixed the PATH

--- src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/GameController.java
+++ src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/GameController.java
@@ -16,7 +16,7 @@ public class GameController extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/InGameWindow.fxml"));
         Scene scene = new Scene(root);
-        javafx.scene.text.Font.loadFont(getClass().getResourceAsStream("Style/fonts/Source_Code_Pro/SourceCodePro-ExtraLight.ttf"), 10);
+        javafx.scene.text.Font.loadFont(getClass().getResourceAsStream("/Style/fonts/Source_Code_Pro/SourceCodePro-ExtraLight.ttf"), 10);
         scene.getStylesheets().add(getClass().getResource("/Style/ingame.css").toExternalForm());
         scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> log.info("Width: " + newSceneWidth));
         scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> log.info("Height: " + newSceneHeight));
