737e292f7ae20ac5983b6de3097ef38c2b1d7d83
Schwabe Kai <ks204@hdm-stuttgart.de>
2019-12-06 18:01:12 +0100

    [CLASS] changed MainController: added Scene Changing and adjusted some stuff [DISCLAIMER] USER LABEL NAME CHANGE STILL NEEDS TO BE SOLVED [FXML] changed StartWindow: added OnAction and outsourced User to UserData.fxml

--- src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
+++ src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/MainController.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,11 +16,14 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.IOException;
 
 public class MainController extends Application {
 
+    //Logger
     private static final Logger log = LogManager.getLogger(MainController.class);
 
+    //Setting FXML Elements
     @FXML
     public Button mainStartButton;
     @FXML
@@ -32,7 +36,10 @@ public class MainController extends Application {
     public Label mainUserNameHintLabel;
     @FXML
     public Label userID;
+    @FXML
+    public Label gameUserID;
 
+    //Setting Scene
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/StartWindow.fxml"));
@@ -42,38 +49,65 @@ public class MainController extends Application {
         setWindow(primaryStage, scene, log);
     }
 
+    //Setting Stage
     static void setWindow(Stage primaryStage, Scene scene, Logger log) {
 
-        scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> log.info("Width: " + newSceneWidth));
-        scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> log.info("Height: " + newSceneHeight));
+        scene.widthProperty().addListener((observableValue, oldSceneWidth, newSceneWidth) -> log.debug("Width: " + newSceneWidth));
+        scene.heightProperty().addListener((observableValue, oldSceneHeight, newSceneHeight) -> log.debug("Height: " + newSceneHeight));
+        primaryStage.setTitle("inquiz");
         primaryStage.setMinWidth(650);
         primaryStage.setMinHeight(650);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    //Starting Stage
     public static void main(String[] args) {
         launch(args);
     }
 
+    //Event Handler
+    @FXML
     public void mainExitAction(ActionEvent actionEvent) {
         Runtime.getRuntime().exit(0);
     }
-    public void mainStatsAction(ActionEvent actionEvent) {
 
+    @FXML
+    public void mainStatsAction(ActionEvent actionEvent) throws IOException {
+        Parent gameView = FXMLLoader.load(getClass().getResource("/fxml/Statistics.fxml"));
+        Scene gameViewScene = new Scene(gameView);
+        Stage gameStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        gameStage.setScene(gameViewScene);
+        gameStage.show();
     }
-    public void mainStartAction(ActionEvent actionEvent) {
-        if (mainUserNameTextField.getText().length() > 0){
-            userID.setText(mainUserNameTextField.getText());
+
+    @FXML
+    public void mainEnterStartAction(ActionEvent actionEvent) throws IOException {
+        validateUserName(actionEvent);
+    }
+
+    @FXML
+    public void mainStartAction(ActionEvent actionEvent) throws IOException {
+        validateUserName(actionEvent);
+    }
+
+    //Other outsourced Functions
+    private void validateUserName(ActionEvent actionEvent) throws IOException {
+        if (mainUserNameTextField.getText().length() > 0) {
+            //USER ID STILL NEEDS TO NE CHANGED !!!
+            //String userIDString = mainUserNameTextField.getText();
             RotateTransition rotateTransition = new RotateTransition();
             rotateTransition.setDelay(Duration.millis(1500));
             rotateTransition.setDuration(Duration.millis(900));
             rotateTransition.setByAngle(720);
-            rotateTransition.setNode(userID);
+            rotateTransition.setNode(gameUserID);
             rotateTransition.play();
-            mainStartButton.setText("success");
-        }
-        else{
+            Parent gameView = FXMLLoader.load(getClass().getResource("/fxml/InGameWindow.fxml"));
+            Scene gameViewScene = new Scene(gameView);
+            Stage gameStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            gameStage.setScene(gameViewScene);
+            gameStage.show();
+        } else {
             mainUserNameHintLabel.setText("please enter a username");
             mainUserNameHintLabel.setTextFill(Color.web("#FF0000"));
         }
