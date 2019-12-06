d56ea1f39e87e1904d72307d5672ec2a6ebdf073
Schwabe Kai <ks204@hdm-stuttgart.de>
2019-12-06 18:02:36 +0100

    [CLASS] added StatisticsController: initialized [FXML] added Statistics: initialized, UI to be added

---
+++ src/main/java/de/hdm_stuttgart/mi/Gui/ViewHandler/StatisticsController.java
@@ -0,0 +1,40 @@
+package de.hdm_stuttgart.mi.Gui.ViewHandler;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public class StatisticsController extends Application {
+
+    private static final Logger log = LogManager.getLogger(MainController.class);
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Statistics.fxml"));
+        Scene scene = new Scene(root);
+        javafx.scene.text.Font.loadFont(getClass().getResourceAsStream("/Style/fonts/Source_Code_Pro/SourceCodePro-ExtraLight.ttf"), 10);
+        scene.getStylesheets().add(getClass().getResource("/Style/ingame.css").toExternalForm());
+        MainController.setWindow(primaryStage, scene, log);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void gameExitAction(ActionEvent actionEvent) throws IOException {
+        Parent gameView = FXMLLoader.load(getClass().getResource("/fxml/StartWindow.fxml"));
+        Scene gameViewScene = new Scene(gameView);
+        Stage gameStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        gameStage.setScene(gameViewScene);
+        gameStage.show();
+    }
+
+}
