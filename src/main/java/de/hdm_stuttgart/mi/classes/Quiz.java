f9a2e236f932b84af0c5779883d0ea520f60ed81
Filippo Matraxia <fm080@hdm-stuttgart.de>
2019-12-13 08:51:52 +0100

    [CLASS] Added Methode getRandomIndicies

--- src/main/java/de/hdm_stuttgart/mi/classes/Quiz.java
+++ src/main/java/de/hdm_stuttgart/mi/classes/Quiz.java
@@ -3,6 +3,9 @@ package de.hdm_stuttgart.mi.classes;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 @XmlRootElement
 public class Quiz {
 
@@ -25,6 +28,26 @@ public class Quiz {
 
     int getLength() {
         return questions.size();
+    }
+
+    int[] getQuestionIndices (){
+        int[] indices = new int [10];
+        Random random = new Random();
+        XMLParser xmlParser = new XMLParser();
+        Quiz quiz = xmlParser.createQuestions();
+
+        for(int i=0; i<10; i++){
+            int randomNumber = random.nextInt(quiz.getLength());
+            indices[i] = randomNumber;
+            for (int j = 1 ; j<=i - 1; j++){
+                if (randomNumber == indices[j-1]){
+                    indices[i] = 0;
+                    i--;
+                    break;
+                }
+            }
+        }
+        return indices;
     }
 }
 
