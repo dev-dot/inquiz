e7fa4e936fb0f3c87d17d7a064cbd5cd34e8b8bf
Anton Richter <ar140@hdm-stuttgart.de>
2019-12-19 10:50:38 +0100

    [CLASS] deleted unused variables

--- src/main/java/de/hdm_stuttgart/mi/classes/AppTest.java
+++ src/main/java/de/hdm_stuttgart/mi/classes/AppTest.java
@@ -6,18 +6,11 @@ import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class AppTest extends Application {
 
 
-    private static final Logger log = LogManager.getLogger(App.class);
-    private static final Scanner input = new Scanner(System.in);
-    private static int roundCounter = 10;
-    private static XMLParser parser = new XMLParser();
-    static Random random = new Random();
-    public int[] questionsIndices = new Quiz().getQuestionIndices();
+    private static final Logger log = LogManager.getLogger(AppTest.class);
+
 
 
     public static void main(String[] args) {
