77ae62d6d0f43ad692a13e68f9b0816a77c25126
Anton Richter <ar140@hdm-stuttgart.de>
2019-12-19 10:55:50 +0100

    [CLASS] init Logger in Quiz class

--- src/main/java/de/hdm_stuttgart/mi/classes/Quiz.java
+++ src/main/java/de/hdm_stuttgart/mi/classes/Quiz.java
@@ -1,16 +1,19 @@
 package de.hdm_stuttgart.mi.classes;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 @XmlRootElement
 public class Quiz {
 
     private ArrayList<Question>questions;
-
+    private int[] indices = new int[10];
+    private final Logger log = LogManager.getLogger(Quiz.class);
     public Quiz(){
     }
 
@@ -31,7 +34,8 @@ public class Quiz {
     }
 
     int[] getQuestionIndices (){
-        int[] indices = new int [10];
+
+        log.info("a quiz indecies array was created");
         Random random = new Random();
         XMLParser xmlParser = new XMLParser();
         Quiz quiz = xmlParser.createQuestions();
